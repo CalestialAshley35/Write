c4f5c44bf4b02c46b8b2f97ea80c9527a5236a9d
Calestial Ashley <calestialashley@gmail.com>
2024-09-27 13:02:39 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function runCode() {
+    const htmlCode = document.getElementById("htmlCode").value;
+    const cssCode = "<style>" + document.getElementById("cssCode").value + "</style>";
+    const jsCode = "<script>" + document.getElementById("jsCode").value + "<\/script>";
+    const output = document.getElementById("output").contentWindow.document;
+    output.open();
+    output.write(htmlCode + cssCode + jsCode);
+    output.close();
+}
